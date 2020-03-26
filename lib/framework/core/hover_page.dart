3a31d25c77b5eae92d1f620e974a9723d46937fe
Ernest Epistola <ernestepistola@gmail.com>
2020-03-27 07:53:35 +0800

    Updated to use HoverGlobalWidgets

--- lib/framework/core/hover_page.dart
+++ lib/framework/core/hover_page.dart
@@ -11,28 +11,50 @@ abstract class HoverPage extends HoverRoute {
   static const Duration _defaultSnackBarDuration = Duration(seconds: 2);
 
   HoverPage({
-    this.title: "",
+    this.title,
     this.backgroundColor,
   });
 
-  Widget buildContent(BuildContext context, ScaffoldState scaffoldState);
-  AppBar buildAppBar(BuildContext context, ScaffoldState scaffoldState);
-  Drawer buildDrawer(BuildContext context, ScaffoldState scaffoldState);
-  SnackBar buildSnackBar(BuildContext context, ScaffoldState scaffoldState);
-  Widget buildFloatingActionButton(BuildContext context, ScaffoldState scaffoldState);
+  /// Build the content of the page by overriding this method.
+  Widget buildContent(BuildContext context);
+
+  AppBar buildAppBar(BuildContext context) {
+    if (title != null) {
+      return AppBar(
+        title: Text(title),
+      );
+    }
+    return null;
+  }
+
+  Drawer buildDrawer(BuildContext context) {
+    return null;
+  }
+
+  SnackBar buildSnackBar(BuildContext context) {
+    return null;
+  }
+
+  Widget buildFloatingActionButton(BuildContext context) {
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
-    final globalElements = Provider.of<HoverGlobalWidgets>(context, listen: false);
+    final globalWidgets = Provider.of<HoverGlobalWidgets>(context, listen: false);
+
+    final appBar = buildAppBar(context);
+    final drawer = buildDrawer(context);
+    final fab = buildFloatingActionButton(context);
 
     return SafeArea(
       child: Scaffold(
         backgroundColor: backgroundColor,
         key: scaffoldKey,
-        appBar: buildAppBar(context, scaffoldState),
-        body: buildContent(context, scaffoldState),
-        drawer: buildDrawer(context, scaffoldState),
-        floatingActionButton: buildFloatingActionButton(context, scaffoldState),
+        body: buildContent(context),
+        appBar: (appBar != null) ? appBar : globalWidgets.appBar,
+        drawer: (drawer != null) ? drawer : globalWidgets.drawer,
+        floatingActionButton: (fab != null) ? fab : globalWidgets.floatingActionButton,
       ),
     );
   }
@@ -51,21 +73,28 @@ abstract class HoverPage extends HoverRoute {
 
   ScaffoldState get scaffoldState => scaffoldKey.currentState;
 
+  ///Utility method to get the device's screen width
   double getScreenWidth(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return width;
   }
 
+  ///Utility method to get the device's screen height
   double getScreenHeight(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     return height;
   }
 
   void showSnackBar(BuildContext context, String message, {Duration duration: _defaultSnackBarDuration}) {
-    final SnackBar _snackBar = (buildSnackBar(context, scaffoldState) != null)
-        ? buildSnackBar(context, scaffoldState)
-        : SnackBar(content: Text(message), duration: duration);
-    this.scaffoldKey.currentState.showSnackBar(_snackBar);
+    //If the buildSnackBar method was overidden and doesn't return null,
+    //use the snackbar created from that method. Else, use globalWidgets.snackBar.
+    final globalWidgets = Provider.of<HoverGlobalWidgets>(context, listen: false);
+    var snackBar = buildSnackBar(context);
+    snackBar = (snackBar != null) ? snackBar : globalWidgets.snackBar;
+
+    //If both the builSnackBar() and globalWidgets.snackBar are null, use a basic snackbar.
+    final _snackBar = (snackBar != null) ? snackBar : SnackBar(content: Text(message), duration: duration);
+    scaffoldState.showSnackBar(_snackBar);
   }
 
   HoverRouter _getAppNavigationManager(BuildContext context) {
