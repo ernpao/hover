dc991ae8893d0ff7f24222054173382657642db9
Ernest Epistola <ernestepistola@gmail.com>
2020-03-27 21:04:55 +0800

    Added export statements

--- lib/widgets/hover_widgets.dart
+++ lib/widgets/hover_widgets.dart
@@ -1,1 +1,14 @@
 library hover_widgets;
+
+// Basic buttons
+export 'buttons/call_to_action_button.dart';
+export 'buttons/circle_icon_button.dart';
+
+//Social Media buttons
+export 'buttons/social/facebook_sign_in_button.dart';
+export 'buttons/social/google_sign_in_button.dart';
+
+//Custom Widgets
+export 'custom/full_page_spinner.dart';
+export 'custom/rectangular_image.dart';
+export 'custom/square_image.dart';
