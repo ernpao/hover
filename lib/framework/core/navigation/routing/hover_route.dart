04f52b60179a69e7ea13935ad5f4f1b628dec147
Ernest Epistola <ernestepistola@gmail.com>
2020-03-28 04:34:38 +0800

    Changed to StatefulWidget

--- lib/framework/core/navigation/routing/hover_route.dart
+++ lib/framework/core/navigation/routing/hover_route.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-abstract class HoverRoute extends StatelessWidget {
+abstract class HoverRoute extends StatefulWidget {
   String get routeName;
   GlobalKey<ScaffoldState> get scaffoldKey;
 }
