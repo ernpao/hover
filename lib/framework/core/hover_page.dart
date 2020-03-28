29c312c5357c9fc4e06ff4ed4289c649942c545b
Ernest Epistola <ernestepistola@gmail.com>
2020-03-29 04:06:28 +0800

    Moved buildPageContent and build to HoverPageBase

--- lib/framework/core/hover_page.dart
+++ lib/framework/core/hover_page.dart
@@ -6,27 +6,4 @@ abstract class HoverPage extends HoverPageBase {
     String title,
     Color backgroundColor,
   }) : super(title: title, backgroundColor: backgroundColor);
-
-  Widget buildPageContent(BuildContext context);
-
-  /// Build the content of the page by overriding this method.
-  @override
-  Widget build(BuildContext context) {
-    final globalWidgets = getGlobalWidgets(context);
-
-    final appBar = buildAppBar(context);
-    final drawer = buildDrawer(context);
-    final fab = buildFloatingActionButton(context);
-
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: backgroundColor,
-        key: scaffoldKey,
-        body: buildPageContent(context),
-        appBar: (appBar != null) ? appBar : globalWidgets.appBar,
-        drawer: (drawer != null) ? drawer : globalWidgets.drawer,
-        floatingActionButton: (fab != null) ? fab : globalWidgets.floatingActionButton,
-      ),
-    );
-  }
 }
--- lib/framework/core/hover_page_base.dart
+++ lib/framework/core/hover_page_base.dart
@@ -17,6 +17,29 @@ abstract class HoverPageBase extends HoverScaffold implements HoverRoute, HoverN
     return Provider.of<HoverGlobalWidgets>(context, listen: false);
   }
 
+  Widget buildPageContent(BuildContext context);
+
+  /// Build the content of the page by overriding this method.
+  @override
+  Widget build(BuildContext context) {
+    final globalWidgets = getGlobalWidgets(context);
+
+    final appBar = buildAppBar(context);
+    final drawer = buildDrawer(context);
+    final fab = buildFloatingActionButton(context);
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: backgroundColor,
+        key: scaffoldKey,
+        body: buildPageContent(context),
+        appBar: (appBar != null) ? appBar : globalWidgets.appBar,
+        drawer: (drawer != null) ? drawer : globalWidgets.drawer,
+        floatingActionButton: (fab != null) ? fab : globalWidgets.floatingActionButton,
+      ),
+    );
+  }
+
   void showSnackBar(BuildContext context, String message, {Duration duration: _defaultSnackBarDuration}) {
     //If the buildSnackBar method was overidden and doesn't return null,
     //use the snackbar created from that method. Else, use globalWidgets.snackBar.
