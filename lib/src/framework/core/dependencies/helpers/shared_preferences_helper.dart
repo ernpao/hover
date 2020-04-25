30450d9f493a493b4773261b69b6357ec90a49e4
Ernest Epistola <ernestepistola@gmail.com>
2020-04-25 23:43:34 +0800

    Functions for saving and loading string data to shared preferences

--- lib/src/framework/core/dependencies/helpers/shared_preferences_helper.dart
+++ lib/src/framework/core/dependencies/helpers/shared_preferences_helper.dart
@@ -2,4 +2,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesHelper {
   static Future<SharedPreferences> getInstance() async => SharedPreferences.getInstance();
+
+  static void saveSetting(String key, String value) async {
+    final sharedPreferences = await getInstance();
+    sharedPreferences.setString(key, value);
+  }
+
+  Future<String> loadSetting(String key) async {
+    final sharedPreferences = await getInstance();
+    return sharedPreferences.getString(key);
+  }
 }
