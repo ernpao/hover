a79fe671f7a6dd2a94db656e9953ceac83e03a47
Ernest Epistola <ernestepistola@gmail.com>
2020-04-25 23:57:48 +0800

    Store instance instead of repeatedly calling getInstance

--- lib/src/framework/core/dependencies/helpers/shared_preferences_helper.dart
+++ lib/src/framework/core/dependencies/helpers/shared_preferences_helper.dart
@@ -1,7 +1,14 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesHelper {
-  static Future<SharedPreferences> getInstance() async => SharedPreferences.getInstance();
+  static SharedPreferences _instance;
+
+  static Future<SharedPreferences> getInstance() async {
+    if (_instance == null) {
+      _instance = await SharedPreferences.getInstance();
+    }
+    return _instance;
+  }
 
   static void saveSetting(String key, String value) async {
     final sharedPreferences = await getInstance();
