54149c8e3d62dab2af391e56c78b2f54a378e835
Ernest-Epistola <ernestepistola@gmail.com>
2020-10-25 21:56:43 +0800

    Added documentation for HoverRoundImage

--- lib/src/widgets/custom/hover_round_image.dart
+++ lib/src/widgets/custom/hover_round_image.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HoverRoundImage extends StatelessWidget {
+  /// URL of the image to display
   final String imageUrl;
+
+  /// Radius size of the widget
   final double radius;
+
+  /// How to inscribe the image into the space allocated during layout.
   final BoxFit fit;
   HoverRoundImage({
     @required this.imageUrl,
