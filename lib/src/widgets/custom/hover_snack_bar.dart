2283a112331cafb6ac727193a1b76aec07d02f11
Ernest-Epistola <ernestepistola@gmail.com>
2020-10-25 22:41:06 +0800

    Added documentation for HoverSnackBar.

--- lib/src/widgets/custom/hover_snack_bar.dart
+++ lib/src/widgets/custom/hover_snack_bar.dart
@@ -5,10 +5,19 @@ class HoverSnackBar extends SnackBar {
   static const Duration defaultDuration =
       HoverSnackBarHelper.defaultSnackBarDuration;
 
+  /// The widget to display inside the snack bar.
+  final Widget content;
+
+  /// Sets the how long the snack bar will be displayed.
+  final Duration duration;
+
+  /// Sets the width of the widget.
+  final double width;
+
   HoverSnackBar({
-    @required Widget content,
-    Duration duration: defaultDuration,
-    double width,
+    @required this.content,
+    this.duration,
+    this.width,
   }) : super(
           duration: duration,
           elevation: 0,
