72b5e719e92096c0a71ff0cd5ec0e3d918b95789
Ernest-Epistola <ernestepistola@gmail.com>
2020-10-25 22:58:56 +0800

    Added documentation for HoverSquareImage and HoverSquareImageCard.

--- lib/src/widgets/custom/hover_square_image.dart
+++ lib/src/widgets/custom/hover_square_image.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HoverSquareImage extends StatelessWidget {
+  /// URL of the image to display.
   final String imageUrl;
+
+  /// Size of the widget.
   final double size;
+
+  /// How to inscribe the image into the space allocated during layout.
   final BoxFit fit;
   HoverSquareImage({
     @required this.imageUrl,
@@ -22,18 +27,30 @@ class HoverSquareImage extends StatelessWidget {
 }
 
 class HoverSquareImageCard extends StatelessWidget {
+  /// URL of the image to display.
   final String imageUrl;
+
+  /// Size of the widget.
   final double size;
+
+  /// Sets the corner radius of the widget.
   final double borderRadius;
+
+  /// The z-coordinate at which to place this card. This controls the size of
+  /// the shadow below the card.
+  final double elevation;
+
   HoverSquareImageCard({
     @required this.imageUrl,
     @required this.size,
     this.borderRadius: 0,
+    this.elevation: 1,
   });
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: elevation,
       clipBehavior: Clip.antiAlias,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(borderRadius),
